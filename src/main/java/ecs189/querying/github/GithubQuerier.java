ecb5b19f43bffee0132b0d9b7275d84aed24fdd1
Soham K <sohamkoradia@gmail.com>
2017-03-15 21:02:08 -0700

    Made the enhancements to retrieve the 10 last pushed events. Also redesigned the UI to make it more user friendly.

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -19,8 +19,14 @@ public class GithubQuerier {
 
     private static final String BASE_URL = "https://api.github.com/users/";
 
+    // Used for filtering different event types
+    private static final String PUSH_EVENT = "PushEvent";
+    private static final String FORK_EVENT = "ForkEvent";
+
     public static String eventsAsHTML(String user) throws IOException, ParseException {
-        List<JSONObject> response = getEvents(user);
+        // Get all the relevant events that we are interested in
+        List<JSONObject> response = getEvents(user, PUSH_EVENT);
+
         StringBuilder sb = new StringBuilder();
         sb.append("<div>");
         for (int i = 0; i < response.size(); i++) {
@@ -34,33 +40,146 @@ public class GithubQuerier {
             Date date = inFormat.parse(creationDate);
             String formatted = outFormat.format(date);
 
-            // Add type of event as header
-            sb.append("<h3 class=\"type\">");
-            sb.append(type);
-            sb.append("</h3>");
-            // Add formatted date
-            sb.append(" on ");
-            sb.append(formatted);
-            sb.append("<br />");
-            // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
-            sb.append("<a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a>");
-            sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
-            sb.append(event.toString());
-            sb.append("</pre> </div>");
+            // Wrap this particular event's info in a container
+            sb.append("<div class=\"container\">");
+                // And for stylistic purposes, a jumbotron
+                sb.append("<div class=\"jumbotron eventJumbo\">");
+
+                    // Use Bootstrap's grid system for positioning the elements properly
+                    sb.append("<div class=\"row\">");
+
+                        // Three columns by themselves
+                        sb.append("<div class=\"col-md-3\"></div>");
+
+                        // And three for the event label
+                        sb.append("<div class=\"col-md-4\" id=\"typeCol\">");
+
+                            // Add type of event as header
+                            sb.append("<span class=\"type withLatoBlack\">");
+
+                                // Adapt text according to the type it is
+                                String userFriendlyTypeLabel;
+                                String repoName = event.getJSONObject("repo").getString("name");
+
+                                // If the repo name (along with the account name) is too long, shorten it so that it properly displays
+                                if (repoName.length() > 23) {
+                                    repoName = repoName.substring(0, 21 - (repoName.length() - repoName.indexOf('/') + 1)) + "..." + repoName.substring(repoName.indexOf("/"), repoName.length());
+                                }
+
+                                if (type.equals(PUSH_EVENT)) {
+                                    userFriendlyTypeLabel = "Pushed <span class=\"withLatoLight\">to</span> " + "<span class=\"label label-primary\" id=\"repoLabel\">" + repoName + "</span>";
+                                } else {
+                                    userFriendlyTypeLabel = "Unknown Type of Event.";
+                                }
+                                sb.append(userFriendlyTypeLabel);
+
+                            sb.append("</span>");
+
+                        // Close the above div
+                        sb.append("</div>");
+
+                        // And three for the time label
+                        sb.append("<div class=\"col-md-4\" id=\"dateCol\">");
+
+                            // Add formatted date
+                            // Add type of event as header
+                            sb.append("<span class=\"date withOxygenRegular\">");
+                                sb.append("<span class=\"label label-default\">");
+                                    sb.append(formatted);
+                                sb.append("</span>");
+                            sb.append("</span>");
+
+                        // Close the above div
+                        sb.append("</div>");
+
+                        // The remaining three columns by themselves
+                        sb.append("<div class=\"col-md-3\"></div>");
+
+                    // Close the row div
+                    sb.append("</div>");
+
+                    if (type.equals(PUSH_EVENT)) {
+                        // Now display the SHA hashes of the commits
+                        sb.append("<hr class=\"commitIntroHR\">");
+                        sb.append("<p class=\"withLatoRegular center\">Here are the commits associated with this push request:</p>");
+
+                        // Get the Payload as a JSONObject
+                        JSONObject payload = event.getJSONObject("payload");
+                        JSONArray commitArray = payload.getJSONArray("commits");
+
+                        for (int commitIndex = 0; commitIndex < commitArray.length(); commitIndex++) {
+                            JSONObject thisCommit = commitArray.getJSONObject(commitIndex);
+                            String shaOfCommit = thisCommit.getString("sha");
+                            String urlOfCommit = thisCommit.getString("url");
+
+                            sb.append("<p class=\"withMontserratRegular center sha\"><a href=\"" + urlOfCommit + "\">");
+                            sb.append(shaOfCommit);
+                            sb.append("</a></p>");
+                        }
+                    }
+
+                    sb.append("<br />");
+                    // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
+                    sb.append("<p class=\"center\" id=\"rawJSONLbl\"><a data-toggle=\"collapse\" href=\"#event-" + i + "\">Raw JSON</a></p>");
+                    sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
+                    sb.append(event.toString());
+                    sb.append("</pre> </div>");
+
+                // Close the jumbotron div
+                sb.append("</div>");
+            // And the container one as well
+            sb.append("</div>");
         }
         sb.append("</div>");
         return sb.toString();
     }
 
-    private static List<JSONObject> getEvents(String user) throws IOException {
+    private static List<JSONObject> getEvents(String user, String... eventsToCapture) throws IOException {
         List<JSONObject> eventList = new ArrayList<JSONObject>();
-        String url = BASE_URL + user + "/events";
-        System.out.println(url);
-        JSONObject json = Util.queryAPI(new URL(url));
-        System.out.println(json);
-        JSONArray events = json.getJSONArray("root");
-        for (int i = 0; i < events.length() && i < 10; i++) {
-            eventList.add(events.getJSONObject(i));
+
+        // Bool to keep track of whether the next page should be explored
+        boolean continueTraversing = true;
+
+        // Page number to traverse through - starting at 1
+        int pageNum = 1;
+
+        // Other initialization variables
+        int numOfPushEvents = 0;
+        boolean validType;
+
+        while (numOfPushEvents < 10 && continueTraversing) {
+            String url = BASE_URL + user + "/events";
+            System.out.println(url);
+            JSONObject json = Util.queryAPI(new URL(url));
+            System.out.println(json);
+            JSONArray events = json.getJSONArray("root");
+
+            // Check to make sure that the JSON response was not empty
+            if (events.length() == 0) {
+                continueTraversing = false;
+                break;
+            }
+
+            for (int eventIndex = 0; eventIndex < events.length(); eventIndex++) {
+                JSONObject thisEvent = events.getJSONObject(eventIndex);
+                String thisEventType = thisEvent.getString("type");
+                validType = false;
+
+                for (String eachRelevantType : eventsToCapture) {
+                    if (eachRelevantType.equals(thisEventType)) {
+                        validType = true;
+                        break;
+                    }
+                }
+
+                // Only select events that are relevant
+                if (validType) {
+                    eventList.add(thisEvent);
+                    numOfPushEvents++;
+                }
+            }
+
+            pageNum++;
         }
         return eventList;
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -12,35 +12,45 @@
     <link rel='stylesheet' href='webjars/bootstrap/3.2.0/css/bootstrap.min.css'>
     <link rel='stylesheet' href='main.css'>
     <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-    <title>Github Stalker</title>
+    <title>GitHub Activity Monitor</title>
   </head>
   <body>
   <div id="core">
-    <h2>What Have I Been Up To?</h2>
-    <form action="index.jsp" method="GET">
-      <div class="form-group">
-        <label for="user"><h3>Github username:</h3></label>
-        <input type="user" name="user_name" class="form-control" id="user"
-               value="<%= request.getParameter("user_name") == null ? "" : request.getParameter("user_name")%>">
+    <div class="jumbotron" id="header">
+      <h2 class="center withLatoBlack">GitHub <span class="withLatoLight">Activity Monitor</span></h2>
+    </div>
+    <%String user=request.getParameter("user_name"); %>
+
+    <div id="mainContentViewport">
+      <% if (user == null || user.isEmpty()) { %>
+        <form action="index.jsp" method="GET">
+          <div class="form-group">
+            <label for="user"><h3>Github Username:</h3></label>
+            <input type="user" name="user_name" class="form-control" id="user">
+          </div>
+          <button type="submit" class="btn btn-default">
+            <c:choose>
+              <c:when test="${empty user}">
+                Submit
+              </c:when>
+              <c:otherwise>
+                Refresh
+              </c:otherwise>
+            </c:choose>
+          </button>
+        </form>
+      <% } else { %>
+        <h3 class="withOxygenLight center" id="activityDescriptor">Here's what <span class="withLatoBlack"><%= user %></span> has been up to.</h3>
+        <br />
+      <% } %>
+
+      <div id="activity">
+        <% if (user != null && !user.isEmpty()) { %>
+            <%= GithubQuerier.eventsAsHTML(user) %>
+          <% } else { %>
+              Tell me who you are, and I will tell you what you did last ... week? Month? Summer? Not sure yet.
+          <% } %>
       </div>
-      <%String user=request.getParameter("user_name"); %>
-      <button type="submit" class="btn btn-default">
-        <c:choose>
-          <c:when test="${empty user}">
-            Submit
-          </c:when>
-          <c:otherwise>
-            Refresh
-          </c:otherwise>
-        </c:choose>
-      </button>
-    </form>
-    <div id="activity">
-      <%if (user != null && !user.isEmpty()){%>
-          <%=GithubQuerier.eventsAsHTML(user)%>
-        <% } else { %>
-            Tell me who you are, and I will tell you what you did last ... week? Month? Summer? Not sure yet.
-        <% }%>
     </div>
   </div>
 
