7dfb6d2c41e39bbc622afb2aea004009db2b5b4a
Kevin Ow <k3vin15243@gmail.com>
2017-03-16 20:13:33 -0700

    Added Swap 1

--- hw4-hotswap/nginx-rev/swap1.sh
+++ hw4-hotswap/nginx-rev/swap1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #/bin/bash
-# This shell is to swap from web1 to web2
+# This shell is to swap from web2 to web1
 cd /etc/nginx
-sed -e s?web1:8080/activity/?web2:8080/activity/? <nginx.conf > /tmp/xxx
+sed -e s?web2:8080/activity/?web1:8080/activity/? <nginx.conf > /tmp/xxx
 cp /tmp/xxx nginx.conf
 service nginx reload
